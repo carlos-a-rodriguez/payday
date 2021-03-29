40a81b8c84936ef6691077f683280201af2f7832
Carlos A. Rodriguez <charlie.mojocoa@gmail.com>
2021-03-29 11:59:26 -0400

    add basic API template

---
+++ payday/api.py
@@ -0,0 +1,14 @@
+"""payday APIs"""
+import datetime
+
+
+def is_pay_day(date: datetime.date) -> bool:
+    pass
+
+
+def next_pay_day(date: datetime.date) -> datetime.date:
+    pass
+
+
+def previous_pay_day(date: datetime.date) -> datetime.date:
+    pass
---
+++ tests/payday/test_api.py
@@ -0,0 +1,13 @@
+"""API unit tests"""
+import unittest
+
+
+class APITestCase(unittest.TestCase):
+    def test_is_pay_day(self):
+        self.fail("Not Implemented Yet!")
+
+    def test_next_pay_day(self):
+        self.fail("Not Implemented Yet!")
+
+    def test_previous_pay_day(self):
+        self.fail("Not Implemented Yet!")
