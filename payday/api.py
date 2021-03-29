f1df4848ea591e3747d6d6c9f2dbb3db3c043ce0
Carlos A. Rodriguez <charlie.mojocoa@gmail.com>
2021-03-29 16:39:08 -0400

    calculate whether supplied date is pay day

--- payday/api.py
+++ payday/api.py
@@ -1,14 +1,81 @@
 """payday APIs"""
+import calendar
 import datetime
+import numpy as np
+from payday.lib.holidays.bank import USBankHolidays
+
+
+MID_MONTH_DAY = 15
+
+
+def adjusted_eom_pay_day(year: int, month: int) -> datetime.date:
+    return np.busday_offset(
+        unadjusted_eom_pay_day(year, month).strftime("%Y-%m-%d"),
+        0,
+        roll="preceding",
+        holidays=us_bank_holidays(year),
+    ).astype(datetime.date)
+
+
+def adjusted_mom_pay_day(year: int, month: int) -> datetime.date:
+    return np.busday_offset(
+        unadjusted_mom_pay_day(year, month).strftime("%Y-%m-%d"),
+        0,
+        roll="preceding",
+        holidays=us_bank_holidays(year),
+    ).astype(datetime.date)
+
+
+def eom_day(year: int, month: int) -> int:
+    _, day = calendar.monthrange(year, month)
+    return day
+
+
+def us_bank_holidays(year: int) -> np.ndarray:
+    holidays = USBankHolidays(years=[year])
+    return np.array(
+        [
+            np.datetime64(
+                date.strftime("%Y-%m-%d")
+            )
+            for date, _ in holidays.items()
+        ]
+    )
+
+
+def unadjusted_mom_pay_day(year: int, month: int) -> datetime.date:
+    return datetime.date(year, month, day=MID_MONTH_DAY)
+
+
+def unadjusted_eom_pay_day(year: int, month: int) -> datetime.date:
+    return datetime.date(
+        year,
+        month,
+        eom_day(year, month)
+    )
+
+
+def _is_pay_day(date: datetime.date) -> bool:
+    if date.day > MID_MONTH_DAY:
+        return date == adjusted_eom_pay_day(date.year, date.month)
+    return date == adjusted_mom_pay_day(date.year, date.month)
+
+
+def _next_pay_day(date: datetime.date) -> datetime.date:
+    pass
+
+
+def _previous_pay_day(date: datetime.date) -> datetime.date:
+    pass
 
 
 def is_pay_day(date: datetime.date) -> bool:
-    pass
+    return _is_pay_day(date)
 
 
 def next_pay_day(date: datetime.date) -> datetime.date:
-    pass
+    return _next_pay_day(date)
 
 
 def previous_pay_day(date: datetime.date) -> datetime.date:
-    pass
+    return _previous_pay_day(date)
--- tests/payday/test_api.py
+++ tests/payday/test_api.py
@@ -1,13 +1,90 @@
 """API unit tests"""
+import datetime
 import unittest
+
+import payday.api
 
 
 class APITestCase(unittest.TestCase):
-    def test_is_pay_day(self):
-        self.fail("Not Implemented Yet!")
+    def test_is_pay_day_eom_day(self):
+        self.assertTrue(
+            payday.api.is_pay_day(
+                datetime.date(2021, 3, 31)
+            )
+        )
+
+    def test_is_pay_day_eom_holiday(self):
+        self.assertFalse(
+            payday.api.is_pay_day(
+                datetime.date(2021, 5, 31)
+            )
+        )
+
+    def test_is_pay_day_eom_holiday_adjustment(self):
+        self.assertTrue(
+            payday.api.is_pay_day(
+                datetime.date(2021, 5, 28)
+            )
+        )
+
+    def test_is_pay_day_mom_day(self):
+        self.assertTrue(
+            payday.api.is_pay_day(
+                datetime.date(2021, 1, 15)
+            )
+        )
+
+    def test_is_pay_day_mom_holiday(self):
+        self.assertFalse(
+            payday.api.is_pay_day(
+                datetime.date(2021, 2, 15)
+            )
+        )
+
+    def test_is_pay_day_mom_holiday_adjustment(self):
+        self.assertTrue(
+            payday.api.is_pay_day(
+                datetime.date(2021, 2, 12)
+            )
+        )
 
     def test_next_pay_day(self):
         self.fail("Not Implemented Yet!")
 
     def test_previous_pay_day(self):
         self.fail("Not Implemented Yet!")
+
+    def test_unadjusted_eom_pay_day(self):
+        today = datetime.date(2021, 5, 1)
+        self.assertEqual(
+            payday.api.unadjusted_eom_pay_day(today.year, today.month),
+            today.replace(day=31)
+        )
+
+    def test_unadjusted_eom_pay_day_december(self):
+        today = datetime.date(2021, 12, 1)
+        self.assertEqual(
+            payday.api.unadjusted_eom_pay_day(today.year, today.month),
+            today.replace(day=31)
+        )
+
+    def test_unadjusted_eom_pay_day_feb_leap_year(self):
+        today = datetime.date(2020, 2, 1)
+        self.assertEqual(
+            payday.api.unadjusted_eom_pay_day(today.year, today.month),
+            today.replace(day=29)
+        )
+
+    def test_unadjusted_eom_pay_day_feb_non_leap_year(self):
+        today = datetime.date(2021, 2, 1)
+        self.assertEqual(
+            payday.api.unadjusted_eom_pay_day(today.year, today.month),
+            today.replace(day=28)
+        )
+
+    def test_unadjusted_mom_pay_day(self):
+        today = datetime.date(2020, 2, 1)
+        self.assertEqual(
+            payday.api.unadjusted_mom_pay_day(today.year, today.month),
+            today.replace(day=15)
+        )
