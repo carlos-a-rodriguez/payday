38c7dd45693264f1e85cbbddf36adc44892b8fb3
Carlos A. Rodriguez <charlie.mojocoa@gmail.com>
2021-08-25 20:50:09 -0400

    fix List type annotation; format document

--- payday/api.py
+++ payday/api.py
@@ -1,7 +1,7 @@
 """payday APIs"""
 import datetime
 from functools import lru_cache
-from typing import Generator
+from typing import Generator, List
 
 from dateutil.rrule import MONTHLY, rrule
 import numpy as np
@@ -10,7 +10,7 @@ from payday.lib.holidays.bank import USBankHolidays
 
 
 def adjusted_date(date: datetime.date) -> datetime.date:
-    """ date adjusted for weekends and holidays """
+    """date adjusted for weekends and holidays"""
     return np.busday_offset(
         dates=date,
         offsets=0,
@@ -20,23 +20,23 @@ def adjusted_date(date: datetime.date) -> datetime.date:
 
 
 @lru_cache(maxsize=16)
-def bank_holidays(year: int) -> [datetime.date]:
-    """ all the bank holidays for a given year """
+def bank_holidays(year: int) -> List[datetime.date]:
+    """all the bank holidays for a given year"""
     return [holiday for holiday in USBankHolidays(years=[year]).keys()]
 
 
 def is_pay_day(date: datetime.date) -> bool:
-    """ is date a pay day? """
+    """is date a pay day?"""
     return next_pay_day(date) == date
 
 
 def next_pay_day(date: datetime.date) -> datetime.date:
-    """ next pay day from the date provided (inclusive) """
+    """next pay day from the date provided (inclusive)"""
     return next(pay_days_gen(date=date))
 
 
 def pay_days_gen(date: datetime.date) -> Generator[datetime.date, None, None]:
-    """ all pay days from start (inclusive) """
+    """all pay days from start (inclusive)"""
     for dt in rrule(freq=MONTHLY, bymonthday=(15, -1), dtstart=date):
         if (pay_day := adjusted_date(dt.date())) >= date:
             yield pay_day
